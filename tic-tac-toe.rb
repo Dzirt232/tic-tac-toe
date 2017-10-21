4c406a5fe459c689b35ced2de6a676cec8d5ff7b
Dzirt232 <dzirt232@rambler.ru>
2017-10-21 19:13:02 +0300

    Game arena created

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -7,6 +7,14 @@ class Game
     def initialize(name)
       @name = name
     end
+
+    def self.won?
+      return false
+    end
+
+    def xod(position)
+
+    end
   end
 
   def self.new_player(name="player_#{@@players}")
@@ -15,7 +23,20 @@ class Game
   end
 
   class Pole
+    @@pole = [1,2,3,4,5,6,7,8,9]
+    def self.show_pole
+      puts
+      @@pole.each { |e|
+        print "| #{e} | "
+        print "\n\n" if e % 3 == 0 && e >= 3
+      }
+      puts
+    end
+  end
 
+  def self.game_over?
+    return true if Player.won?
+    return false
   end
 end
 
@@ -27,3 +48,17 @@ puts "Введите имя второго игрока: "
 name_2 = gets.chomp
 player_2 = name_2 == "" ? Game.new_player() : Game.new_player(name_2)
 puts "Добро пожаловать в игру, #{player_1.name} и #{player_2.name}!"
+
+until Game.game_over?
+  puts "Ход игрока #{player_1.name}"
+  position = gets.chomp.to_i
+  player_1.xod(position)
+  Game::Pole.show_pole
+  break until Game.game_over?
+  puts "Ход игрока #{player_2.name}"
+  position = gets.chomp.to_i
+  player_2.xod(position)
+  Game::Pole.show_pole
+end
+
+puts "#{player_1} выиграл!!!"
